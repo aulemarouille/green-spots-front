6f90beb02d59a85be753a94d9cd73d66ad6efcd5
au.lemarouille <au.lemarouille@gmail.com>
2025-08-26 15:33:07 +0200

    Delete useless import

--- src/services/spots.service.ts
+++ src/services/spots.service.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosError } from "axios";
 import { SpotsResponse, TypesResponse } from "@/types/spots";
+import axios from "axios";
 
 const baseUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/spots/`;
 
